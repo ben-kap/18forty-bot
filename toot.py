22961ba72a8e6311ea065bb815c836ffb1158bcc
Ben K <79790398+ben-kap@users.noreply.github.com>
2023-12-23 18:40:09 +0000

    fixed character limit bug and added Jewish History in Parsha feed

--- toot.py
+++ toot.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from mastodon import Mastodon
 import feedparser
 from bs4 import BeautifulSoup
+import textwrap
 import pickle
 
 dir_name = os.path.dirname(__file__)
@@ -15,11 +16,11 @@ mastodon = Mastodon(
 d1 = feedparser.parse('https://18forty.org/articles/feed/')
 d2 = feedparser.parse('https://ireadthisovershabbos.substack.com/feed/')
 d3 = feedparser.parse('https://www.spreaker.com/show/4344730/episodes/feed')
+d4 = feedparser.parse('https://readingjewishhistoryintheparsha.substack.com/feed')
 posted_file = dir_name+'/posted.pickle'
 
-for d in [d1,d2,d3]:
+for d in [d1,d2,d3,d4]:
     num_entries = min(len(d.entries),10)
-
     for x in range(num_entries-1,-1,-1):
         entry = d.entries[x]
         link = entry.link
@@ -29,14 +30,17 @@ for d in [d1,d2,d3]:
         else:
             posted_list = []
         if link not in posted_list:
-
-            if 'Shabbos' in d.feed.title:
+            if 'Shabbos' or 'Jewish History' in d.feed.title:
                 desc = BeautifulSoup(entry.description,'lxml').p.text
                 toot = entry.author+':\n\"'+entry.title+'\"\n\n'+desc+'\n\n'+entry.link.split('#')[0]
             elif 'Podcast' in d.feed.title:
                 toot = d.feed.title+':\n'+entry.title+'\n\n'+entry.link.split('#')[0]
             elif 'Articles' in d.feed.title:
-                toot = '18Forty Article:\n\"'+entry.title+'\"\n\n'+entry.link.split('#')[0]   
+                toot = '18Forty Article:\n\"'+entry.title+'\"\n\n'+entry.link.split('#')[0]
+            if len(toot)>500:
+                new_desc_len = len(desc)-(len(toot)-500)
+                desc = textwrap.shorten(desc, width =new_desc_len, placeholder='...')
+                toot = '\"'+entry.title+'\"'+' by '+entry.author+'\n\n'+desc+'\n\n'+entry.link
             mastodon.status_post(toot, visibility='public')
             posted_list.append(link)
             with open(posted_file, 'wb') as f:
